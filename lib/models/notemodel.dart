2dd5c29198e712d2b87db39912a2c698133ab34e
mohamedmostafarizk <mohamed.most1311@gmail.com>
2025-07-15 23:28:36 +0300

    add color to note model

--- lib/models/notemodel.dart
+++ lib/models/notemodel.dart
@@ -2,6 +2,12 @@ class Notemodel {
   final String title;
   final String subtitle;
   final String date;
+  final int color;
 
-  Notemodel({required this.title, required this.subtitle, required this.date});
+  Notemodel({
+    required this.color,
+    required this.title,
+    required this.subtitle,
+    required this.date,
+  });
 }
