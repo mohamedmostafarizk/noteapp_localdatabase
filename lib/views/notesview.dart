65ec4347a5aa7a047e185ca8eb6cc18e1cc995e1
mohamedmostafarizk <mohamed.most1311@gmail.com>
2025-07-15 21:58:18 +0300

    showbottomsheet

--- lib/views/notesview.dart
+++ lib/views/notesview.dart
@@ -1,7 +1,8 @@
-// ignore_for_file: deprecated_member_use, sort_child_properties_last
+// ignore_for_file: deprecated_member_use, sort_child_properties_last, camel_case_types
 
 import 'package:flutter/material.dart';
 import 'package:noteapp/views/Notesbody.dart';
+import 'package:noteapp/widgets/showbottomsheet.dart';
 
 class Notesview extends StatelessWidget {
   const Notesview({super.key});
@@ -10,7 +11,14 @@ class Notesview extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return showbottomsheet();
+            },
+          );
+        },
         child: const Icon(Icons.add),
         backgroundColor: Colors.blue,
       ),
---
+++ lib/widgets/showbottomsheet.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+
+class showbottomsheet extends StatelessWidget {
+  const showbottomsheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(height: 400, child: Center(child: Text("Add a new note")));
+  }
+}
