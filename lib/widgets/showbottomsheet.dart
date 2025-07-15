bdc804146c4ad449c79f83ebab259eac4469e5f4
mohamedmostafarizk <mohamed.most1311@gmail.com>
2025-07-15 23:05:59 +0300

    add space from bottom at bottom sheet

--- lib/widgets/showbottomsheet.dart
+++ lib/widgets/showbottomsheet.dart
@@ -9,56 +9,59 @@ class showbottomsheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
-      child: Column(
-        children: [
-          const SizedBox(height: 16),
-          const Text(
-            "Add a new note",
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 16),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 16),
+            const Text(
+              "Add a new note",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  hintText: "Note title",
                 ),
-                hintText: "Note title",
               ),
             ),
-          ),
-          const SizedBox(height: 16),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: TextField(
-              maxLines: 8,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
+            const SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                maxLines: 8,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  hintText: "content",
                 ),
-                hintText: "content",
               ),
             ),
-          ),
-          const SizedBox(height: 16),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            height: 35,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(12),
-            ),
+            const SizedBox(height: 16),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              height: 35,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(12),
+              ),
 
-            child: Center(
-              child: Text(
-                'Add',
-                style: TextStyle(color: Colors.black, fontSize: 18),
+              child: Center(
+                child: Text(
+                  'Add',
+                  style: TextStyle(color: Colors.black, fontSize: 18),
+                ),
               ),
             ),
-          ),
-        ],
+            SizedBox(height: 16),
+          ],
+        ),
       ),
     );
   }
